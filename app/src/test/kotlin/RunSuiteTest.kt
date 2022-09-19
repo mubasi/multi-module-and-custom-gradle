b8d75c1724af6d80bf8255eaefad8b60c6477a99
Ali <ali.bana@bluebirdgroup.com>
2022-09-19 14:10:22 +0700

    add RunSuiteDomainPassenger.kt

--- app/src/test/kotlin/RunSuiteTest.kt
+++ app/src/test/kotlin/RunSuiteTest.kt
@@ -1,21 +1,13 @@
-import id.bluebird.vsm.domain.fleet.domain.interactor.GetCountCasesTest
-import id.bluebird.vsm.domain.fleet.domain.interactor.GetListFleetUseCasesTest
-import id.bluebird.vsm.domain.fleet.domain.interactor.RequestFleetUseCasesTest
-import id.bluebird.vsm.domain.fleet.domain.interactor.SearchFleetUseCasesTest
-import id.bluebird.vsm.feature.queue_fleet.main.QueueFleetViewModelTest
-import id.bluebird.vsm.feature.queue_fleet.search_fleet.SearchFleetViewModelTest
+import id.bluebird.vsm.domain.passenger.domain.interactor.RunSuiteDomainPassenger
+import id.bluebird.vsm.domain.user.domain.usescases.RunSuiteDomainUser
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.platform.suite.api.SelectClasses
 import org.junit.platform.suite.api.Suite
 
 @Suite
 @SelectClasses(
-    QueueFleetViewModelTest::class,
-    SearchFleetViewModelTest::class,
-    GetCountCasesTest::class,
-    GetListFleetUseCasesTest::class,
-    RequestFleetUseCasesTest::class,
-    SearchFleetUseCasesTest::class
+    RunSuiteDomainPassenger::class,
+    RunSuiteDomainUser::class,
 )
 @ExperimentalCoroutinesApi
 class RunSuiteTest
