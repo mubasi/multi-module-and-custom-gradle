8fbbff591dee05162360f966a4a8df3534644510
Muhammad Bayu Sasinda <bayusasinda29@gmail.com>
2022-11-11 04:26:04 +0000

    update snackbar use dialoutil local feature
    update snackbar
    
    finish update user management

--- feature/user_management/src/main/kotlin/id/bluebird/vsm/feature/user_management/utils/DialogUtil.kt
+++ feature/user_management/src/main/kotlin/id/bluebird/vsm/feature/user_management/utils/DialogUtil.kt
@@ -1,14 +1,33 @@
 package id.bluebird.vsm.feature.user_management.utils
 
 import android.content.Context
+import android.text.Spanned
+import android.view.Gravity
 import android.view.LayoutInflater
+import android.view.View
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
 import id.bluebird.vsm.feature.user_management.R
 import id.bluebird.vsm.feature.user_management.databinding.ActionBottomViewBinding
 
 
 object DialogUtil {
+
+    fun showSnackbar(view: View, message: Spanned, color: Int){
+        val snackbar = Snackbar.make(view, message, Snackbar.LENGTH_LONG)
+        val layoutParams = LinearLayout.LayoutParams(snackbar.view.layoutParams)
+
+        layoutParams.gravity = Gravity.TOP
+        layoutParams.setMargins(-10,160,-10,0)
+        snackbar.view.layoutParams = layoutParams
+        snackbar.animationMode = BaseTransientBottomBar.ANIMATION_MODE_FADE
+        snackbar.view.setBackgroundColor(ContextCompat.getColor(view.context, color))
+        snackbar.show()
+    }
 
     fun actionDialogUser(
         context: Context, title: String?, message: String, user: String,
