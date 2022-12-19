247eb3978a058065bb1574f7f90a3f4180378862
Ali <daulay.alibana@gmail.com>
2022-12-19 10:13:13 +0700

    - update version to 1.1.1 and code version to 11

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -1,19 +1,18 @@
 object Version {
-    private const val major = 2
-    private const val minor = 2
-    private const val patch = 1
-
     val minSdk by lazy { 26 }
     val compileSdk by lazy { 32 }
     val targetSdk by lazy { 32 }
 
-    val versionType by lazy { "" }
+    private val major by lazy { 1 }
+    private val minor by lazy { 1 }
+    private val patch by lazy { 1 }
+
+    private val versionType by lazy { "" }
 
     val appName by lazy { "VSM Pangkalan" }
     val appNameStaging by lazy { "$appName (Staging)" }
     val appNameDev by lazy { "$appName (Dev)" }
 
-    val versionName by lazy { "1.1.0$versionType" }
-    val versionCode by lazy { 9 }
-    val appVersion by lazy {"$major.$minor.$patch"}
+    val versionName by lazy { "$major.$minor.$patch$versionType" }
+    val versionCode by lazy { 11 }
 }
