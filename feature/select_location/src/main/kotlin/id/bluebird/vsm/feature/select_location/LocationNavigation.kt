a751c9990afcdb3c3813dc0ddca01059c43c7722
ali <ali.bana@bluebirdgroup.com>
2023-03-03 17:14:32 +0700

    update unittest getsublocationqrCode - add test for QueueFleetViewModel.kt - add env information

--- feature/select_location/src/main/kotlin/id/bluebird/vsm/feature/select_location/LocationNavigation.kt
+++ feature/select_location/src/main/kotlin/id/bluebird/vsm/feature/select_location/LocationNavigation.kt
@@ -1,10 +1,17 @@
 package id.bluebird.vsm.feature.select_location
 
+import androidx.annotation.VisibleForTesting
+import id.bluebird.vsm.core.extensions.StringExtensions
 import id.bluebird.vsm.feature.select_location.model.LocationNavigation
 
 object LocationNavigationTemporary {
 
     private var _locationNav: LocationNavigation? = null
+    private var isOnTesting = false
+    @VisibleForTesting
+    const val locationName = "sample"
+    @VisibleForTesting
+    const val subLocationName = "sub ${StringExtensions.SUFFIX_TEST}"
 
     fun updateLocationNav(locationNavigation: LocationNavigation?) {
         _locationNav = locationNavigation
@@ -14,7 +21,26 @@ object LocationNavigationTemporary {
         _locationNav = null
     }
 
-    fun getLocationNav(): LocationNavigation? = _locationNav
+    fun getLocationNav(): LocationNavigation? {
+        return if(!isOnTesting){
+            LocationNavigation(
+                locationName = "sample"
+            )
+        } else {
+            _locationNav
+        }
+    }
 
-    fun isLocationNavAvailable(): Boolean = _locationNav != null
+    fun isLocationNavAvailable(): Boolean {
+        return if(isOnTesting){
+            return false
+        } else {
+            _locationNav != null
+        }
+    }
+
+    @VisibleForTesting
+    fun setTestingVariable(isOnTesting : Boolean){
+        this.isOnTesting = isOnTesting
+    }
 }
